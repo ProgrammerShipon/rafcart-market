2e4e414271c593782e2bf86f60e69b20454f2be5
Shipon Hossen Raju <msshipon234@gmail.com>
2022-10-21 06:29:32 +0600

    project setup , test and make header , nav styling

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dist/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: " 1200px"
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors: {
+        'primary': '#FD3D57'
+      }
+    },
+  },
+  plugins: [],
+}
